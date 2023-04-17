ce53c832e6f86e060c973b5d5667c4a5d009c475
kql <769156974@qq.com>
2023-04-17 14:20:25 +0800

    fjyt网关基本搭建

---
+++ fjyt-gateway/src/main/java/com/fjyt/gateway/GateWayApplication.java
@@ -0,0 +1,25 @@
+package com.fjyt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author keQiLong
+ * @date 2023年04月14日 17:30
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  FJYT网关启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------   -------  --    --  ------- \n" +
+                " |             |     ''    ''     |\n" +
+                " |             |      '' ''       |\n" +
+                " |------       |       '''        |\n" +
+                " |             |        |         |\n" +
+                " |             |        |         |\n" +
+                " |        ''   |        |         |\n" +
+                " |         ''  |        |         |\n" +
+                " '          ''''        '         '");
+    }
+}
