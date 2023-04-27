911b495036b9e0e421ad782efec5a66d8cff9263
kql <769156974@qq.com>
2023-04-27 09:21:47 +0800

    拦截器

---
+++ fjyt-common/src/main/java/com/fjyt/common/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.fjyt.common.config;
+
+import com.fjyt.common.interceptor.HeaderInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author keQiLong
+ * @date 2023年04月27日 9:18
+ * 拦截器配置
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    /** 不需要拦截地址 */
+    public static final String[] excludeUrls = { "/login", "/logout", "/refresh" };
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        registry.addInterceptor(getHeaderInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(excludeUrls)
+                .order(-10);
+    }
+
+    /**
+     * 自定义请求头拦截器
+     */
+    public HeaderInterceptor getHeaderInterceptor()
+    {
+        return new HeaderInterceptor();
+    }
+}
